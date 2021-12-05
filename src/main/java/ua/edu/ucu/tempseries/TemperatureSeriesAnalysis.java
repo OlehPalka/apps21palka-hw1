53c58fc390ea0b8d19e860873f40b677925e6bec
OlehPalka <palka280617@gmail.com>
2021-12-05 22:41:50 +0200

    style changes

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -6,7 +6,7 @@ import java.util.InputMismatchException;
 public class TemperatureSeriesAnalysis {
     private double[] temperatureSeries;
     private int actualLength = 0;
-    private static final int theLowest = -273;
+    private final static int theLowest = -273;
 
     public TemperatureSeriesAnalysis() {
         this.temperatureSeries = new double[] {};
