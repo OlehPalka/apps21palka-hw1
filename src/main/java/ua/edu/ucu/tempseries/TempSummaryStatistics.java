02027bef1da4aeb31203c6a85339339ab1b65076
OlehPalka <palka280617@gmail.com>
2021-12-05 23:06:03 +0200

    style changes

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -3,6 +3,9 @@ package ua.edu.ucu.tempseries;
 public class TempSummaryStatistics {
     private double avgTemp;
     private double devTemp;
+    private double minTemp;
+    private double maxTemp;
+
 
     public double getAvgTemp() {
         return avgTemp;
@@ -19,9 +22,6 @@ public class TempSummaryStatistics {
     public double getMaxTemp() {
         return maxTemp;
     }
-
-    private double minTemp;
-    private double maxTemp;
 
     public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp) {
         this.avgTemp = avgTemp;
